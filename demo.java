c4bbc6584f32b04300809f6043824af480de8376
vishalbhati-115 <vbtest115@gmail.com>
2026-02-26 14:31:13 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,5 +2,8 @@ public class demo{
     public static void main(String[]args){
         System.out.print("hello java");
  System.out.println("My name is luffy");
+        for(i=0;i<5;i++){
+            System.out.println(i);
+            
     }
 }
