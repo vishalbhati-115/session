bc322389debc82ca9cdca2621fd603d0b5b9b449
vishal <vishal@gmail.com>
2026-02-26 14:25:54 +0530

    File change

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo{
     public static void main(String[]args){
         System.out.print("hello java");
- System.out.print("My name is luffy");
+ System.out.println("My name is luffy");
     }
 }
