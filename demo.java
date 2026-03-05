5242812ba5e3aec28313c42e869636e54771896e
vishalbhati-115 <vbtest115@gmail.com>
2026-03-05 14:51:49 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class demo{
     public static void main(String[]args){
         System.out.print("hi java kya kar rahe ho");
- System.out.println("Aryan Bhai Kya Kar Rahe Ho");
+ System.out.println("Aryan Bhai Kya Kar Rahe Ho kya aquib ");
        
     }
 }
