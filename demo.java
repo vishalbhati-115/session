4e3d1eb44ad323589ad71b3b38ab801578bcc043
vishalbhati-115 <vbtest115@gmail.com>
2026-03-11 21:27:34 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class demo{
     public static void main(String[]args){
         System.out.println("hi java");
- System.out.println("Hello vishal  ");
+ System.out.println("Hello vishal hii  ");
        
     }
 }
