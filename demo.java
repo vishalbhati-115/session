87e95fb50de94cf8966fd8b90b17740616077de3
vishalbhati-115 <vbtest115@gmail.com>
2026-03-11 21:14:02 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class demo{
     public static void main(String[]args){
         System.out.println("hi java");
- System.out.println("Hello Jenkinks vishal this side and i am fine WHAT ARE YOU DOYING  ");
+ System.out.println("Hello Jenkinks  ");
        
     }
 }
